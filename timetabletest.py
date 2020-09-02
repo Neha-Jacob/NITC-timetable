7aaa7829770a8d02720896c3f1e9434b72377ef7
Neha-Jacob <60283339+Neha-Jacob@users.noreply.github.com>
2020-09-02 14:45:31 +0530

    Add files via upload

---
+++ timetabletest.py
@@ -0,0 +1,64 @@
+def subjects (a,b,c,d,e,f,g,h,p,q,r,s,t,u):
+    sub={"A":a,"B":b,"C":c,"D":d,"E":e,"F":f,"G":g,"H":h,"G@":g,"A+":a,"H+":h,"G+":g,"E+":e,"F+":f,"C+":c,"B+":b,"E@":e,"P":p,"Q":q,"R":r,"S":s,"T":t,"U":u}
+    return sub
+
+def OriginalTime():
+    table=[["A","F","D","B","G","P/E+","P","P","H"],["B","G","E","C","A@","Q/F+","Q","Q","H"],["C","A","F","D","H","R/G+","R","R","E@"],["D","B","G","E","----","S/C+","S","S","G@"],["E","C","A","F","H+","T","T","T","D+"]]
+    return table
+
+def daywise(day,sub):
+    #sub=subjects('a','b','c','d','e','f','g','h','p','q','r','s','t','u')
+    table=OriginalTime()
+    ind=0
+    timing=["8am","9am","10:15am","11:15am","1pm","2pm","3pm","4pm","5pm"]
+    for i in range(9):
+        if table[day][i] == None or table[day][i]=='----':
+            print (timing[i],": ----")
+        else:
+            if "@" in table[day][i]:
+                ind=(table[day][i]).find('@')
+                if ind==3:
+                    x=(table[day][i])[0:1]
+                    y=(table[day][i])[2:3]
+                    print (timing[i],": ",sub[x],", extra-> ",sub[y])
+                else:
+                    x=(table[day][i])[0:1]
+                    print (timing[i],": extra-> ",sub[x])
+
+            else:
+                if "+" in table[day][i]:
+                    ind=(table[day][i]).find('+')
+                    if ind==3:
+                        x=(table[day][i])[0:1]
+                        y=(table[day][i])[2:3]
+                        print (timing[i],": ",sub[x],", extra-> ",sub[y])
+                    else:
+                        x=(table[day][i])[0:1]
+                        print (timing[i],": extra-> ",sub[x])
+                else:
+                    print (timing[i],": ",sub[table[day][i]])
+
+
+def main():
+    print ("Type ur subjects. For empty codes type '----' ")
+    a=input("A. ")
+    b=input("B. ")
+    c=input("C. ")
+    d=input("D. ")
+    e=input("E. ")
+    f=input("F. ")
+    g=input("G. ")
+    h=input("H. ")
+    p=input("P. ")
+    q=input("Q. ")
+    r=input("R. ")
+    s=input("S. ")
+    t=input("T. ")
+    u=input("U. ")
+    sub=subjects(a,b,c,d,e,f,g,h,p,q,r,s,t,u)
+    print("Enter the day option:'\n'1. Monday'\n'2. Tuesday'\n'3. Wednesday'\n'4. Thursday'\n'5. Friday")
+    day=int(input())
+    daywise(day-1,sub)
+
+if __name__=="__main__":
+    main()
