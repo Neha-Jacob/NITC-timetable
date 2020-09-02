5fbde490effca3c9e4a43b6ccdb2f48b0ef37158
lloydjk <60283326+lloyd-jk@users.noreply.github.com>
2020-09-02 13:03:57 +0530

    Add files via upload

--- timetable.py
+++ timetable.py
@@ -1,61 +1,40 @@
-def subjects (a,b,c,d,e,f,g,h,p,q,r,s,t,u):
-    sub={"A":a,"B":b,"C":c,"D":d,"E":e,"F":f,"G":g,"H":h,"G@":g,"A+":a,"H+":h,"G+":g,"E+":e,"F+":f,"C+":c,"B+":b,"E@":e,"P":p,"Q":q,"R":r,"S":s,"T":t,"U":u}
+def subjects (a,b,c,d,e,f,g,h,p,q,r,s,t):
+    sub={"A":a,"B":b,"C":c,"D":d,"E":e,"F":f,"G":g,"H":h,"P":p,"Q":q,"R":r,"S":s,"T":t}
     return sub
 
-def OriginalTime():
-    table=[["A","F","D","B","G","P","P","P","P","H"],["B","G","E","C","A@","Q","Q","Q","Q","H"],["C","A","F","D","H","R","R","R","R","E@"],["D","B","G","E","----","S","S","S","S","G@"],["E","C","A","F","H+","T","T","T","D+"]]
+def OriginalTime(A,B,C,D,E,F,G,H,P,Q,R,S,T):
+    table=[[A,F,D,B,G,E,P,P,H],[B,G,E,C,A,Q,Q,Q,H],[C,A,F,D,H,R,R,R,E],[D,B,G,E,"----",S,S,S,G],[E,C,A,F,H,T,T,T,D]]
     return table
 
-def daywise(day,sub):
-    #sub=subjects('a','b','c','d','e','f','g','h','p','q','r','s','t','u')
-    table=OriginalTime()
-    timing=["8am","9am","10:15am","11:15am","1pm","2pm","3pm","4pm","5pm"]
-    for i in range(9):
-        if table[day][i] == None or table[day][i]=='----':
-            print (timing[i],": ----")
-        else:
-            if "@" in table[day][i]:
-                print (timing[i],": -@- ",sub[table[day][i]])
-            else:
-                if "+" in table[day][i]:
-                    print (timing[i],": -+- ",sub[table[day][i]])
-                else:
-                    print (timing[i],":",sub[table[day][i]])
+def putting_subjects():
+    a=input("Slot A:")
+    b=input("Slot B:")
+    c=input("Slot C:")
+    d=input("Slot D:")
+    e=input("Slot E:")
+    f=input("Slot F:")
+    g=input("Slot G:")
+    h=input("Slot H:")
+    p=input("Slot P:")
+    q=input("Slot Q:")
+    r=input("Slot R:")
+    s=input("Slot S:")
+    t=input("Slot T:")
+    return a,b,c,d,e,f,g,h,p,q,r,s,t
 
+
+def daywise(day):
+    print("Enter your subjects for your required slots and type 'None' for others")
+    a,b,c,d,e,f,g,h,p,q,r,s,t=putting_subjects()
+    sub=subjects(a,b,c,d,e,f,g,h,p,q,r,s,t)
+    table=OriginalTime(sub["A"],sub["B"],sub["C"],sub["D"],sub["E"],sub["F"],sub["G"],sub["H"],sub["P"],sub["Q"],sub["R"],sub["S"],sub["T"])
+    list1=["8-9",   "9-10" ,  "10:15-11:15","11:15-12:15" ,  "1-2"  , "2-3" ,  "3-4"  , "4-5"  ,"5-6"]
+    for i in range(9):
+        print (list1[i],table[day-1][i])
 def main():
-    print ("Typee ur subjects with code")
-    print ("A. ")
-    a=input()
-    print ("B. ")
-    b=input()
-    print ("C. ")
-    c=input()
-    print ("D. ")
-    d=input()
-    print ("E. ")
-    e=input()
-    print ("F. ")
-    f=input()
-    print ("G. ")
-    g=input()
-    print ("H. ")
-    h=input()
-    print ("P. ")
-    p=input()
-    print ("Q. ")
-    q=input()
-    print ("R. ")
-    r=input()
-    print ("S. ")
-    s=input()
-    print ("T. ")
-    t=input()
-    print ("U. ")
-    u=input()
-    sub=subjects(a,b,c,d,e,f,g,h,p,q,r,s,t,u)
     print("Enter the day option:'\n'1. Monday'\n'2. Tuesday'\n'3. Wednesday'\n'4. Thursday'\n'5. Friday")
     day=int(input())
-    daywise(day+1,sub)
+    daywise(day)
 
 if __name__=="__main__":
     main()
